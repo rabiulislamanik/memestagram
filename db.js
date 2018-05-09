831970da2e0925a019f6b4fd109ac94bdf9b3bbf
Rabiul Islam Anik <skywrath.slash@gmail.com>
2018-05-09 12:01:38 +0600

    moved db.js to config folder

--- db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-var connection = mysql.createConnection({
-  host     : '127.0.0.1',
-  user     : 'root',
-  password : '',
-  database : 'esn'
-});
-module.exports = connection;
