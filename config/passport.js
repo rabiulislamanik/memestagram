e9d59746bf965246f8077596e05fdf8bb9283a1d
Rabiul Islam Anik <skywrath.slash@gmail.com>
2018-05-17 13:59:26 +0600

    added passport.js in the config folder.

---
+++ config/passport.js
@@ -0,0 +1,30 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const db = require('../config/db.js');
+const keys = require('../config/keys.js');
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretkey;
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      console.log(jwt_payload);
+      db.query(`SELECT * from users where user_id = ${db.escape(jwt_payload.id)}`,(err,rows,fields)=>{
+        if(!err){
+          const user = rows[0];
+          if(rows[0]){
+            return done(null, user);
+          }
+          else{
+            return done(null,false);
+          }
+        }
+        else{
+          res.status(400).send(err);
+        }
+      });
+    })
+  );
+};
