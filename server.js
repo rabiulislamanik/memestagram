9d990ddd69f1cbfbcdde8a8e294a6fd017f37fa0
Rabiul Islam Anik <skywrath.slash@gmail.com>
2018-05-17 16:01:32 +0600

    Added passport middleware.

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const db = require('./config/db.js');
+const passport = require('passport');
 
+const db = require('./config/db.js');
 const test = require('./routes/api/test.js');
 const users = require('./routes/api/users.js');
 
+//bodyparser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//
 app.use('/test',test);
 app.use('/users',users);
 
-app.get('/',(req,res,err)=>{
-  //console.log(db);
-  db.query('SELECT * from users',(err,rows,fields)=>{
-    if(!err){
-      //console.log(rows);
-      res.status(200).send(rows[0]);
-    }
-    else{
-      res.send(err);
-    }
-  });
-});
-
-
+//passport middleware
+app.use(passport.initialize());
+require('./config/passport.js')(passport);
+//
 port = process.env.PORT || 3000 ;
 app.listen(port ,()=>{
   console.log(`Server is running on port : ${port}`);
