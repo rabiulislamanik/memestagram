f597c9c0da580e75cbe3db7597ccdc277b8d43a2
Rabiul Islam Anik <rabiul.islam2676@gmail.com>
2018-06-01 16:18:21 +0600

    Added code for validating the uploaded files.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -13,7 +13,28 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({ storage: storage ,limits : {fileSize:2000000}}).single('photo');
+const upload = multer({
+  storage: storage ,
+  limits : {fileSize:2000000},
+  fileFilter : function(req,file,cb){
+    validateFile(file,cb);
+  }
+}).single('photo');
+
+function validateFile(file,cb){
+  const filetypes = /png|jpg|jpeg|gif/;
+  const isExtNameValid = filetypes.test(path.extname(file.originalname.toLowerCase()));
+  const isMimeTypeValid = filetypes.test(file.mimetype);
+  // console.log(isExtNameValid);
+  // console.log(file.mimetype);
+  // console.log(isMimeTypeValid);
+  if (isExtNameValid && isMimeTypeValid){
+    cb(null, true);
+  }
+  else{
+    cb('Please upload images only.');
+  }
+}
 
 router.post('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
   upload(req, res, function (err) {
@@ -21,26 +42,30 @@ router.post('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
         // An error occurred when uploading
         return res.status(400).send({file : err});
       }
-      //no error
-
-      //console.log(req.file);
-      //console.log(req.file.filename);
-      req.body.post_text = _.isEmpty( req.body.post_text) ? '' :  req.body.post_text ;
-      const user_id = req.user.user_id;
-      const post_text = req.body.post_text;
-      const post_image_link =req.file.filename ;
-      const query = `INSERT INTO posts (user_id,post_text,post_image_link) VALUES (${db.escape(user_id)},${db.escape(post_text)},${db.escape(post_image_link)}) `;
-
-      db.query(query,(err,rows,fields)=>{
-
-        if(!err){
-          res.status(200).send({post : "post created."});
+      else{
+        //no error
+        //console.log(req.file);
+        //console.log(req.file.filename);
+        if(req.file==undefined){
+          return res.status(400).send({file : "Please select a file."});
         }
-        else{
-          res.status(400).send({alert : "An error occured while creating the post."});
-          console.log(err);
-        }
-      });
+        req.body.post_text = _.isEmpty( req.body.post_text) ? '' :  req.body.post_text ;
+        const user_id = req.user.user_id;
+        const post_text = req.body.post_text;
+        const post_image_link =req.file.filename ;
+        const query = `INSERT INTO posts (user_id,post_text,post_image_link) VALUES (${db.escape(user_id)},${db.escape(post_text)},${db.escape(post_image_link)}) `;
+
+        db.query(query,(err,rows,fields)=>{
+
+          if(!err){
+            res.status(200).send({post : "post created."});
+          }
+          else{
+            res.status(400).send({alert : "An error occured while creating the post."});
+            console.log(err);
+          }
+        });
+      }
   });
 
 });
