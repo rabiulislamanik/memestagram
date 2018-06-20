cb0cde418701bd4ee226e5640630a7a6f563a310
Rabiul Islam Anik <rabiul.islam2676@gmail.com>
2018-06-20 11:21:55 +0600

    Added react-router and layout,auth components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import './App.css';
+
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+import Landing from './components/layouts/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>App</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <Route exact path="/" component={Landing}/>
+          <div className= 'container'>
+            <Route exact path="/Login" component={Login}/>
+            <Route exact path="/Register" component={Register}/>
+          </div>
+          <Footer/>
+        </div>
+      </Router>
     );
   }
 }
