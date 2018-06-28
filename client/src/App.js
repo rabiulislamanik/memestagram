691e96ef1ab9979865a18c47b3c38c3f03ab346b
Rabiul Islam Anik <rabiul.islam2676@gmail.com>
2018-06-28 15:48:48 +0600

    Wrapped router with provider.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import {setCurrentUser} from './actions/authAction';
+import store from './store';
 import './App.css';
 
 import Navbar from './components/layouts/Navbar';
@@ -8,20 +13,29 @@ import Landing from './components/layouts/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
+//check if there is a token 
+if(localStorage.jwtToken){
+  setAuthToken(localStorage.jwtToken);
+  const decodedToken = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decodedToken));
+}
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar/>
-          <Route exact path="/" component={Landing}/>
-          <div className= 'container'>
-            <Route exact path="/Login" component={Login}/>
-            <Route exact path="/Register" component={Register}/>
+      <Provider store={store}> 
+        <Router>
+          <div className="App">
+            <Navbar/>
+            <Route exact path="/" component={Landing}/>
+            <div className= 'container'>
+              <Route exact path="/Login" component={Login}/>
+              <Route exact path="/Register" component={Register}/>
+            </div>
+            <Footer/>
           </div>
-          <Footer/>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
